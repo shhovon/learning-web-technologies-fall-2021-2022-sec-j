6e3405075a25325a8baf5d081cf78efe38a60468
Shovon <shovon.zzz5@gmail.com>
2021-11-08 16:43:14 +0600

    database task added

---
+++ FINAL_LAB_DATABASE/model/db.php
@@ -0,0 +1,16 @@
+<?php 
+	
+	$host 	= "localhost";
+	$dbname = 'webtech';
+	$dbuser = 'root';
+	$dbpassword = '';
+
+	function getConnection(){
+		global $host;
+		global $dbname;
+		global $dbuser;
+		global $dbpassword;
+		
+		$con = mysqli_connect($host, $dbuser, $dbpassword, $dbname);
+		return $con;
+	}
