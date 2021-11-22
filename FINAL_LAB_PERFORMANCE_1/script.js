e944fa14d6cc63685915985f24195c905f84fa64
Shovon <shovon.zzz5@gmail.com>
2021-11-22 16:46:56 +0600

    final commit

--- FINAL_LAB_PERFORMANCE_1/script.js
+++ FINAL_LAB_PERFORMANCE_1/script.js
@@ -1,63 +1,66 @@
-const numberButtons = document.querySelectorAll('[data-number]')
-const operationButtons = document.querySelectorAll('[data-operation]')
-const equalsButton = document.querySelectorAll('[data-equals]')
-const deleteButton = document.querySelectorAll('[data-delete]')
-const allClearbutton = document.querySelectorAll('[data-all-clear]')
-const previousOperandTextElement = document.querySelectorAll('[data-previous-operand]')
-const currentOperandTextElement = document.querySelectorAll('[data-current-operand]')
+const numberButtons = document.querySelectorAll('[data-number]');
+const operationButtons = document.querySelectorAll('[data-operation]');
+const equalsButton = document.querySelectorAll('[data-equals]');
+const deleteButton = document.querySelectorAll('[data-delete]');
+const allClearbutton = document.querySelectorAll('[data-all-clear]');
+const previousOperandTextElement = document.querySelectorAll('[data-previous-operand]');
+const currentOperandTextElement = document.querySelectorAll('[data-current-operand]');
 
 	function appendNumber(number) {
-		if (number == '.' && this.currentOperand.includes('.')) return
-      this.currentOperand = this.currentOperand.toString() + number.toString()
+		if (number == '.' && this.currentOperand.includes('.')){
+      this.currentOperand = this.currentOperand.toString() + number.toString();
+        }
 	}
 
 	function chooseOperation(operation) {
-		if (this.currentOperand === '') return
+		if (this.currentOperand === ''){
 			if (this.previousOperand !== '') {
-				this.compute()
+				this.compute();
 			}
-       this.operation = operation
-       this.previousOperand = this.currentOperand
-       this.currentOperand = ''
+        }
+       this.operation = operation;
+       this.previousOperand = this.currentOperand;
+       this.currentOperand = '';
 	}
 
 	function compute() {
-      const prev = parseFloat(this.previousOperand)
-      const current = parseFloat(this.currentOperand)
-      if (isNaN(prev) || isNaN(current)) return
+      const prev = parseFloat(this.previousOperand);
+      const current = parseFloat(this.currentOperand);
+      if (isNaN(prev) || isNaN(current)){
       	switch (this.operation) {
       		case '+':
-      		 computation = prev + current
+      		 computation = prev + current;
       		 break
       		 case '-':
-      		 computation = prev - current
+      		 computation = prev - current;
       		 break
       		 case 'x':
-      		 computation = prev * current
+      		 computation = prev * current;
       		 break
       		 case '/':
-      		 computation = prev / current
+      		 computation = prev / current;
       		 break
       		 default:
-      		 return
+          }
+      		 alert('something went wrong..');
       	}
-      	this.currentOperand = computation
-      	this.operation = undefined
-      	this.previousOperand = ''
+      	this.currentOperand = '';
+      	this.operation = '';
+      	this.previousOperand = '';
 	}
     
     function clear() {
-		this.currentOperand = ''
-		this.previousOperand = ''
-		this.operation = undefined
+		this.currentOperand = '';
+		this.previousOperand = ''; 
+		this.operation = '';
 	}
 
 	function deleteBtn() {
-      this.currentOperand = this.currentOperand.toString().slice(0, -1)
+      this.currentOperand = this.currentOperand.toString().slice(0, -1);
 	}
 
 	function updateDisplay() {
-       this.currentOperandTextElement.innerText = this.currentOperand
+       this.currentOperandTextElement.innerText = this.currentOperand;
         if (this.operation != null) {
        	 this.previousOperandTextElement.innerText = `${this.previousOperand} ${this.operation}`
        }
@@ -88,6 +91,6 @@ allClearbutton.addEventListener('click', button => {
 })
 
 deleteButton.addEventListener('click', button => {
-	calculator.delete()
+	calculator.deleteBtn()
 	calculator.updateDisplay()
 })
