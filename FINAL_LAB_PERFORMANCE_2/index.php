862a9be2ee04c3333ba5ae726008cd30a791c0a0
Shovon <shovon.zzz5@gmail.com>
2021-11-24 15:58:55 +0600

    final commit

--- FINAL_LAB_PERFORMANCE_2/index.php
+++ FINAL_LAB_PERFORMANCE_2/index.php
@@ -9,9 +9,24 @@ $input1 = $_POST['input1'];
 $input2 = $_POST['input2'];
 $operator = $_POST['operator'];
 $result = '';
-
+if (is_numeric($input1) && is_numeric($input2)) {
+    switch ($operator) {
+        case "+":
+            $result = (float)$input1 + (float)$input2;
+            break;
+        case "-":
+            $result = (float)$input1 - (float)$input2;
+            break;
+        case "*":
+            $result = (float)$input1 * (float)$input2;
+            break;
+        case "/":
+            $result = (float)$input1 / (float)$input2;
+    }
+}
 
 ?>
+<link rel="stylesheet" href="style.css">
 
 <body>
     <div id="page-wrap">
@@ -24,30 +39,17 @@ $result = '';
                 <input type="number" name="input2" id="input2" value="<?php echo $input2; ?>" />
             </p>
             <p> <b>Result</b>
-                <input name="result" value="<?php echo $result; ?>">
+                <input type="number" name="result" value="<?php echo $result; ?>">
             </p>
-            <input type="submit" name="operator" value="Add" />
-            <input type="submit" name="operator" value="Subtract" />
-            <input type="submit" name="operator" value="Multiply" />
-            <input type="submit" name="operator" value="Divide" />
+            <div id="buttons">
+                <input type="submit" name="operator" value="+" />
+                <input type="submit" name="operator" value="-" />
+                <input type="submit" name="operator" value="*" />
+                <input type="submit" name="operator" value="/" />
+            </div>
         </form>
     </div>
-    <style>
-        body {
-            padding: 0;
-            margin: 20px;
-            background-color: lightgray;
-        }
-
-        .page-wrap {
-            display: grid;
-            justify-content: center;
-            align-content: center;
-            min-height: 100vh;
-            grid-template-columns: repeat(4, 100px);
-            grid-template-rows: minmax(120px, auto) repeat(5, 100px);
-        }
-    </style>
+    <script src="script.js"></script>
 </body>
 
 </html>
---
+++ FINAL_LAB_PERFORMANCE_2/script.js
@@ -0,0 +1,1 @@
+const operatorElement = document.getElementById("operator");
